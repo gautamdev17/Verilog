0dba10b11eb17187822324af82a8b54a1b0e56d7
gautamdev17 <gautamd.nxt@gmail.com>
2025-12-07 16:26:38 +0530

    Create request_matrix_design.v

---
+++ 4x4_NOC_ROUTER/REQUEST_MATRIX/request_matrix_design.v
@@ -0,0 +1,9 @@
+//request matrix
+
+module request_matrix (input [4:0] rc0,rc1,rc2,rc3,rc4, output [24:0]reqMat);
+  assign reqMat[4:0] = rc0; // row 1
+  assign reqMat[9:5] = rc1; // row 2
+  assign reqMat[14:10] = rc2;// row 3
+  assign reqMat[19:15] = rc3;// row 4
+  assign reqMat[24:20] = rc4;//row 5
+endmodule
