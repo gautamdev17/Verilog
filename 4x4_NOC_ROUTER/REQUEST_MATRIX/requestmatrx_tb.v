ced12fb1603d0d0da9465223237cb5b9701cddd2
gautamdev17 <gautamd.nxt@gmail.com>
2025-12-10 23:19:42 +0530

    Create requestmatrx_tb.v

---
+++ 4x4_NOC_ROUTER/REQUEST_MATRIX/requestmatrx_tb.v
@@ -0,0 +1,19 @@
+`timescale 1ns/1ps
+
+module tb();
+  reg [4:0] rc0,rc1,rc2,rc3,rc4;
+  wire [24:0]reqMat;
+  
+  request_matrix inst1(rc0,rc1,rc2,rc3,rc4,reqMat);
+  
+  initial begin
+    $monitor("rc0=%h,rc1=%h,rc2=%h,rc3=%h,rc4=%h,reqMat = %h",rc0,rc1,rc2,rc3,rc4,reqMat);
+  end
+  
+  initial begin
+    #0; rc0 = 5'b00010; rc1 = 5'b00001; rc2 = 5'b00010; rc3 = 5'b01000; rc4 = 5'b10000;
+    #10; rc0 = 5'b10000; rc1 = 5'b00100; rc2 = 5'b01000; rc3 = 5'b10000; rc4 = 5'b00010;
+    #10;
+    $finish;
+  end
+endmodule
