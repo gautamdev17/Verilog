0814f3d5c6fa7f5ea8bb2b3cc85baeffd73c06d3
gautamdev17 <gautamd.nxt@gmail.com>
2025-12-11 21:27:49 +0530

    Update fifo_tb.v

--- 4x4_NOC_ROUTER/SYNC_FIFO/fifo_tb.v
+++ 4x4_NOC_ROUTER/SYNC_FIFO/fifo_tb.v
@@ -1,8 +1,8 @@
 // TESTBENCH
 `timescale 1ns/1ps
 module fifo_tb();
-  parameter DATA_WIDTH = 32;
-  parameter DEPTH      = 8;
+  parameter DATA_WIDTH = 4;
+  parameter DEPTH      = 5;
 
   reg clk,rst,rd_en,wr_en;
   reg  [DATA_WIDTH-1:0] data_in;
@@ -18,81 +18,40 @@ module fifo_tb();
   always #5 clk = ~clk;
 
   initial begin
-    $monitor("t=%0t clk=%0b rst=%0b wr=%0b rd=%0b full=%0b empty=%0b valid=%0b data_in=%h data_out=%h",
-             $time, clk, rst, wr_en, rd_en, full, empty, data_valid, data_in, data_out);
+      $monitor("t=%0t clk=%b rst=%b wr=%b rd=%b full=%b empty=%b valid=%b din=%h dout=%h",
+               $time, clk, rst, wr_en, rd_en, full, empty, data_valid, data_in, data_out);
   end
-
+  
   initial begin
+    //reset
     rst    = 1;
-    rd_en  = 0;
     wr_en  = 0;
-    data_in = '0;
+    rd_en  = 0;
+    data_in = 0;
     #20;
     rst = 0;
-
-    #10;
-    wr_en   = 1;
-    rd_en   = 0;
-    data_in = {8{4'b0001}};
-    #10; 
-    wr_en   = 0;
-    data_in = '0;
-
-    #20;
-    rd_en = 1;
-    #10;
-    rd_en = 0;
-
-    for (int i = 0; i < DEPTH; i = i+1) begin
-      wr_en   = 1;
-      rd_en   = 0;
-      data_in = {i[3:0], i[3:0], i[3:0], i[3:0],
-                 i[3:0], i[3:0], i[3:0], i[3:0]};
-      #10;
-    end
-    wr_en   = 0;
-    data_in = '0;
-
-    #20;
-    wr_en   = 1;
-    rd_en   = 0;
-    data_in = {8{4'b1111}};
-    #10;
-    wr_en   = 0;
-    data_in = '0;
-
-    #20;
-    for (int i = 0; i < DEPTH; i = i+1) begin
-      rd_en = 1;
-      wr_en = 0;
-      #10;
-    end
-    rd_en = 0;
-
-    #20;
-    rd_en = 1;
-    #20;
-    rd_en = 0;
-
-
-    #20;
-    wr_en   = 1;
-    rd_en   = 0;
-    data_in = 32'hAAAA_AAAA;
+    // writr a
+    wr_en=1; data_in=4'ha;
     #10;
 
-    wr_en   = 1;
-    rd_en   = 1;
-    data_in = 32'hBBBB_BBBB;
+    // read a
+    wr_en=0; rd_en=1;  data_in=0;
     #10;
 
-    wr_en   = 0;
-    rd_en   = 1;
-    data_in = '0;
-    #20;
+    // fill mem
+    rd_en=0; wr_en=1;
+    data_in=4'h1; #10;
+    data_in=4'h2; #10;
+    data_in=4'h3; #10;
+    data_in=4'h4; #10;
 
-    rd_en = 0;
+    // should be ignored
+    data_in=4'h5; #10;
+
+    // full read
+    wr_en=0; rd_en=1;  data_in=0;
     #50;
+
     $finish;
   end
 endmodule
