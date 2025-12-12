4530561e76de06897c053b2255f45fb965cfa322
gautamdev17 <gautamd.nxt@gmail.com>
2025-12-12 12:53:36 +0530

    Update sync_fifo_design_with_count.v

--- 4x4_NOC_ROUTER/SYNC_FIFO/sync_fifo_design_with_count.v
+++ 4x4_NOC_ROUTER/SYNC_FIFO/sync_fifo_design_with_count.v
@@ -14,10 +14,10 @@ So each mem[i] is one full DATA_WIDTH-bit flit.*/
   reg [ADDR_WIDTH:0]count;
   assign empty = (count==0);
   assign full = (count==DEPTH);
+  assign data_valid = ~empty;
   /*
   this logic works, but it takes in capacity = depth-1 so one mem space is wasted. count logic avoids this.
   assign empty = (rd_ptr == wr_ptr);
-  assign data_valid = ~empty;
   wire [ADDR_WIDTH-1:0] next_wr = (wr_ptr == DEPTH-1) ? 0 : wr_ptr + 1'b1; // hardware synthesis of % is hard, so im conditions instead
   assign full = (next_wr == rd_ptr);*/
   // assign full = ({~wr_ptr[ADDR_WIDTH],wr_ptr[ADDR_WIDTH-1:0]}==rd_ptr); only works for DEPTH = 2^n;
@@ -25,17 +25,27 @@ So each mem[i] is one full DATA_WIDTH-bit flit.*/
   always@(posedge clk) begin//read
     if(rst) begin
       	rd_ptr<=0;
-    	  data_out<=0;
-    		for(i=0;i<DEPTH;i++)
-    			mem[i]<=0;
-        count<=0;
+    	data_out<=0;
+    	for(i=0;i<DEPTH;i++)
+    		mem[i]<=0;
     end
     else if(rd_en && !empty) begin
         data_out<=mem[rd_ptr];
       	rd_ptr <= (rd_ptr == DEPTH-1) ? 0 : rd_ptr + 1'b1;
-        count<=count-1'b1;
       end
   end
+
+  always@(posedge clk) begin // controlling count
+		if(rst)
+			count<=0;
+		else begin
+			case({wr_en&&!full,rd_en&&!empty})
+				2'b10: count<=count+1'b1;
+				2'b01: count<=count-1'b1;
+				default: count <= count;
+			endcase
+		end
+   end
   always@(posedge clk) begin//write
     if(rst) begin
 		wr_ptr<=0;
@@ -43,7 +53,6 @@ So each mem[i] is one full DATA_WIDTH-bit flit.*/
 	else if(wr_en && !full) begin
       mem[wr_ptr]<=data_in;
       wr_ptr <= (wr_ptr == DEPTH-1) ? 0 : wr_ptr + 1'b1;
-      count<=count+1'b1;
     end
   end
 endmodule
