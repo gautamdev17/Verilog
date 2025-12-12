4f49503edbcd44bb18a59631d774baf946c3d929
gautamdev17 <gautamd.nxt@gmail.com>
2025-12-12 13:34:46 +0530

    Update SA_design_new.sv

--- 4x4_NOC_ROUTER/SWITCH_ALLOCATOR/SA_design_new.sv
+++ 4x4_NOC_ROUTER/SWITCH_ALLOCATOR/SA_design_new.sv
@@ -8,19 +8,20 @@ send grants as 3bit binary encoded values since it saves up space and send as se
 
 
 // switched to system verilog since verilog didnt allow dynamic slicing
-// use dynamic part-select base +: width and compute start before the loop
+// use dynamic part-select base +: width
 `timescale 1ns/1ps
 
 module switch_allocator(
   input  logic [24:0] reqMat,
   input  logic clk,
   input  logic rst,
-  output reg [14:0]   selsignal,
-  output reg [4:0]    fifo_pop
+  output reg [14:0] selsignal, //select winners for each direction
+  output reg [4:0] fifo_pop //winnners are popped and next header flit is read for the nxt cycle
+// losers arent popped and have to wait the next clock efge
 );
   //use 15 bit for SEL SIGNAL represent each winner of an output port in 3 bits
   reg [14:0] rrbptr;
-  integer i,j,k;
+  integer i,j,k,f; // f is flag
 
   always_ff @(posedge clk) begin
     if (rst) begin
@@ -30,19 +31,19 @@ module switch_allocator(
     end else begin
       selsignal <= 0;
       fifo_pop <= 0;
-      for (i=0;i<5;i=i+1) begin
-        k = 0;
-        // initialize j from rrbptr using MSB-first dynamic slice (-: 3)
-        for (j = rrbptr[((i+1)*3-1)-:3];k != 5;j = (j==4)?0:j+1) begin : IN_LOOP
+      for (i=0;i<5;i=i+1) begin //go to each direction
+        k=0;f=0;
+        // run from rrb to a full circle
+        for (j = rrbptr[((i+1)*3-1)-:3];k != 5;j=(j==4)?0:j+1) begin
           k = k + 1;
-          if (reqMat[i*5+j] == 1'b1) begin
+          if (reqMat[i*5+j]==1'b1 && !f) begin
             // write slices using MSB-first dynamic part-select
             //bro so im doing dynamic slicing here and this is the format in sv
 			// base-:width for msb:lsb slicing
-            selsignal[((i+1)*3-1)-:3] <= j[2:0];
-            rrbptr[((i+1)*3-1)-:3] <= (j==4)?0:j+1;
-            fifo_pop[i] <= 1'b1;
-            disable IN_LOOP;
+            selsignal[((i+1)*3-1)-:3]<=j[2:0];//assign winner to respective direction
+            rrbptr[((i+1)*3-1)-:3]<=(j==4)?3'b0:j+1;//rrb ptr will change for nxt cucle
+            fifo_pop[i]<=1'b1;//pop the fifo for winners
+            f=1; //stop here running the inner loop, winner found
           end
         end
       end
